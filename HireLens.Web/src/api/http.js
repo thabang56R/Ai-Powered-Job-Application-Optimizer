6934685f4bcc2b412430461c2caa3344d990e053
thabang56R <thabang56@hotmail.com>
2026-03-03 14:43:51 +0200

    Add central apiFetch helper for production API base

--- HireLens.Web/src/api/http.js
+++ HireLens.Web/src/api/http.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const API_BASE =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5159";
+
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: `${API_BASE}/api`,
 });
 
 http.interceptors.request.use((config) => {
   const token = localStorage.getItem("hirelens_token");
 
-  
   if (token) {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
@@ -19,7 +21,6 @@ http.interceptors.request.use((config) => {
 http.interceptors.response.use(
   (res) => res,
   (err) => {
-    
     console.error("API error:", err?.response?.status, err?.response?.data);
     return Promise.reject(err);
   }
